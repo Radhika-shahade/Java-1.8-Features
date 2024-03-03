23f76bb237188a68d103af4b4b87234fadd482ec
Radhika shahade <radhikashahade0804@gmail.com>
2024-03-03 17:06:08 +0530

    java 8 features

---
+++ src/main/java/com/bootcoding/java8features/constructorreference/Student.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java8features.constructorreference;
+
+public class Student {
+    public Student()
+    {
+        System.out.println("creating object of student class");
+    }
+    public void display()
+    {
+        System.out.println("i am student");
+    }
+}
